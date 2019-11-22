d2069ccfacebaf7df204239fe672a998a1ce60ce
Will Quanstrom <wquanstr215@gmail.com>
2019-11-22 08:13:49 -0800

    get npm installed and set up for project

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'CHANGENAME',
+      title: 'doctor-lookup',
       template: './src/index.html',
       inject: 'body'
     })

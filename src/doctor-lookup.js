36e4353660a5c815cf6be3fce506ba5b5b44dfd0
Will Quanstrom <wquanstr215@gmail.com>
2019-11-22 12:25:53 -0800

    get API hooked up and running correctly

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,9 +1,9 @@
 export class DoctorService {
- async getDoctor(inputName) {
+ async getDoctor() {
     try{
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${inputName}&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=smith&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=dc3f74ad9528ed00531de52e8b4e6ff2`)
       let jsonifiedResponse =  await response.json();
-      return jsonifiedResonse;
+      return jsonifiedResponse;
     } catch(error) {
       console.error('Sorry it didnt work ' + error.message);
     }
--- src/main.js
+++ src/main.js
@@ -4,18 +4,24 @@ import './styles.css';
 import $ from 'jquery';
 import { DoctorService } from './doctor-lookup';
 
-
 $(document).ready(function() {
   $('.doctorName').submit(function(event){
-    event.preventDefault;
-    let inputName = $('inputName').val();
-    getDoctor(inputName);
+    event.preventDefault();
+    // let inputName = $('inputName').val();
 
     (async () => {
         let doctorService = new DoctorService();
-        const response = await doctorservice.getDoctor();
+        const response = await doctorService.getDoctor();
         getElements(response);
     })();
+
+    function getElements(response) {
+      console.log(response);
+      for(let i = 0; i < response.data.length; i++) {
+        $('#doctorList').append('<li>' + response.data[i].name + '</li>');
+
+      }
+    }
 
   });
 });

e1620d98fce92195d32b72baad82dd0f30ba0cce
Will Quanstrom <wquanstr215@gmail.com>
2019-11-22 13:45:37 -0800

    get all forms of necessary data from each doctor displaying correctly

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     function getElements(response) {
       console.log(response);
       for(let i = 0; i < response.data.length; i++) {
-        $('#doctorList').append('<li>' + response.data[i].name + '</li>');
+        $('#doctorList').append('<li>' + response.data[i].name + ', ' + response.data[i].visit_address.street + ', ' + response.data[i].location_slug + ', ' + response.data[i].phones[0].number + ', ' + response.data[i].accepts_new_patients + ', ' + response.data[i].doctors_pagination_url + '</li>');
       }
     }
 

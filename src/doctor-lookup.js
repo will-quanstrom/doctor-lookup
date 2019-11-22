5db581061133923c546e96563783e830546bad75
Will Quanstrom <wquanstr215@gmail.com>
2019-11-22 15:04:53 -0800

    set up specialists api and search

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -3,11 +3,25 @@ export class DoctorService {
  async getDoctor(inputName) {
     try{
 
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${inputName}&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${inputName}&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse =  await response.json();
       return jsonifiedResponse;
     } catch(error) {
       console.error('Sorry it didnt work ' + error.message);
+    }
+  }
+}
+
+export class SpecialistService {
+  async getDoctor(query) {
+    try{
+
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=37.773%2C-122.413%2C100&user_location=47.6062%2C-122.3321&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+      console.log(jsonifiedResponse);
+    } catch(error) {
+      console.error('Sorry it didnt work' + error.message);
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { DoctorService } from './doctor-lookup';
+import { DoctorService, SpecialistService } from './doctor-lookup';
 
 $(document).ready(function() {
   $('.doctorName').submit(function(event){
@@ -21,6 +21,19 @@ $(document).ready(function() {
         $('#doctorList').append('<li>' + response.data[i].name + ', ' + response.data[i].visit_address.street + ', ' + response.data[i].location_slug + ', ' + response.data[i].phones[0].number + ', ' + response.data[i].accepts_new_patients + ', ' + response.data[i].doctors_pagination_url + '</li>');
       }
     }
+  });
+  $('.doctorSpecialty').submit(function(event){
+    event.preventDefault();
+    let query = $('#inputCondition').val();
 
+    (async () => {
+      let specialistService = new SpecialistService;
+      const response = await specialistService.getDoctor(query);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      console.log(response);
+    }
   });
 });

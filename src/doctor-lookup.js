b7c784431b0f54134d88f6f207be662c627644e6
Will Quanstrom <wquanstr215@gmail.com>
2019-11-22 13:53:32 -0800

    get api key working through process.env

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -2,8 +2,8 @@
 export class DoctorService {
  async getDoctor(inputName) {
     try{
-      console.log(inputName);
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${inputName}&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=dc3f74ad9528ed00531de52e8b4e6ff2`)
+
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?name=${inputName}&location=47.6062%2C-122.3321%2C100&user_location=47.6062%2C-122.3321&skip=0&limit=10&user_key=${process.env.API_KEY}`)
       let jsonifiedResponse =  await response.json();
       return jsonifiedResponse;
     } catch(error) {
